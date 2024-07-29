59ddbd6f9c71ed760b4fa0d2e6a026cbaaa48f6f
Daniel Yount <TheDanYount@gmail.com>
2024-07-29 20:40:33 +0000

    Added img input functionality-mDid some querying, some error throwing if something isn't found, added an event listener, and if all's well, set the 's src to the input

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
+'use strict';
 /* global */
 const $imgPreview = document.querySelector('.img-to-add');
 const $imgInput = document.querySelector('#photo');
 if (!$imgInput) throw new Error('$imgInput not found!');
 $imgInput.addEventListener('input', previewPhoto);
-
-function previewPhoto(event: Event): void {
-  const eventTarget = event.target as HTMLInputElement;
+function previewPhoto(event) {
+  const eventTarget = event.target;
   if (!$imgPreview) throw new Error('$imgPreview not found!');
   $imgPreview.setAttribute('src', eventTarget.value);
 }
